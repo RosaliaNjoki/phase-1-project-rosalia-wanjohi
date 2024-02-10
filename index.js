9d3c369376a0ee25bd0f5e467ae0b420dcfe02b7
RosaliaNjoki <njosali0@gmail.com>
2024-02-10 21:17:41 +0300

    limited sales up to zero

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-    
+    //post activation
     document.querySelector("#review-form").addEventListener("submit", handleSubmit);
 
     function handleSubmit(e){
@@ -9,7 +9,8 @@ document.addEventListener('DOMContentLoaded', ()=>{
             }
             postReview(commentsObj);
         }
-    
+    let foodObj;
+    let servingQty;
     const ApiUrl = 'https://trackapi.nutritionix.com/v2/search/instant/?query=hamburger';
     const ApiKey =  '1520077eb8d20fad210e515aaf40b538';
     const appId = '5ea05591';
@@ -18,7 +19,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
     title.style.alignItems="center";
     title.textContent = "Welcome to Humburger Resturant";
     document.getElementById("restuarant-title").appendChild(title);
-    
+    //developing card to use to display Menu
     const menuGrid = document.getElementById("food-menu-grid");
 
     const createMenuCard = (foodName, servingUnit, servingQuantity, photo, idTag ) => {
@@ -96,9 +97,6 @@ document.addEventListener('DOMContentLoaded', ()=>{
                 const servingQuantity = `Serving Quantity: ${currentMenu["serving_qty"]}`;
                 const photo = currentMenu.photo.thumb;
                 const idTag = `Food Id: ${currentMenu["tag_id"]}`;
-                
-             
-                
 
                 const menuCard = createMenuCard(
                     foodName, servingUnit, servingQuantity, photo, idTag
@@ -108,7 +106,6 @@ document.addEventListener('DOMContentLoaded', ()=>{
         
         }  
     }  
-       
         catch(error){
             console.error(error);
         }
@@ -116,8 +113,8 @@ document.addEventListener('DOMContentLoaded', ()=>{
     }
 getMenuItems();
 
-function fetchNavDetails(){
-    //making a Get request to obtain first film details
+function fetchOrderDetails(){
+    //making a Get request to obtain first food details
     fetch('http://localhost:3000/food')
   .then((response) => {
     if (!response.ok) {
@@ -125,18 +122,23 @@ function fetchNavDetails(){
     }
     return response.json(); 
   })
-  .then(data => data.forEach(food => displayMenu(food))) 
+  .then(data => {
+    foodObj = data;
+    data.forEach(food => displayMenu(food))
+  }) 
+
 
 }
 
-fetchNavDetails();
-
+fetchOrderDetails();
+//title to direct customers to order
     const subtitle = document.createElement('h3');
     subtitle.style.alignItems="center";
     subtitle.textContent = "Place Your Order";
     document.getElementById("order-title").appendChild(subtitle);
-
+//developing and appending the order sectiion 
  function displayMenu(food){
+    servingQty= food.serving_qty;
    let orderCard = document.createElement("li");
     orderCard.class = "ordercard";
     orderCard.id = "food-id";
@@ -153,32 +155,40 @@ fetchNavDetails();
     </div>
 
     `
+    
 // integrating patch in serving quantity by reducing the value as more orders are placed
     orderCard.querySelector('#order').addEventListener("click", ()=> {
         food.serving_qty-=1;
+        if (food.serving_qty ===0){
+                const button = document.getElementById("order")
+                button.className = "button-sold-out";
+                button.textContent = "Food Sold Out";
+                button.setAttribute("disabled", "");
+        }
     orderCard.querySelector("span").textContent = food.serving_qty;
-        updateServingQuantity(id);
+        updateServingQuantity(foodObj);
         alert('your order will be served in 5 minutes');
+        
          });
     
     document.querySelector("#order-food").appendChild(orderCard);  
 }
 
-displayMenu();
+
 
 //patch method forserving quantity  
-function updateServingQuantity(id){
-    fetch(`http://localhost:3000/food/${id}`, {
-        method: 'PATCH',
-        headers: {
+function updateServingQuantity(foodObj){
+  
+ fetch(`http://localhost:3000/food/${foodObj}`, {
+    method: 'PATCH',
+     headers: {
             'Content-Type': 'application/json'
         },
-        body:JSON.stringify(dataObj)
-    })
-    .then(res=>res.json())
-    .then(data => console.log(data))
-}
-//reviews post method 
+        body:JSON.stringify({serving_qty: servingQty})
+     })
+    
+ }
+// //reviews post method 
 function postReview(commentsObj){
   
     fetch('http://localhost:3000/comments', {
